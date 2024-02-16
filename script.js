4751cda0b1ac9148be661795f9e94421c067dd91
nataliia-v <nataly.verbenskaya@gmail.com>
2024-02-16 17:09:16 +0200

    added animation

--- script.js
+++ script.js
@@ -13,17 +13,30 @@ class Particle {
 
   constructor(effect) {
     this.effect = effect;
-    this.x = Math.random() * this.effect.width;
-    this.y = Math.random() * this.effect.height;
-    this.radius = 15;
+    this.radius = Math.random() * 40 + 5;
+    this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2); // "this.radius +" to full visible particle at the border(left)  " - this.radius * 2" to full visible particle at the border(right)
+    this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2) // "this.radius +" to full visible particle at the border(top)  " - this.radius * 2" to full visible particle at the border(bottom)
+    this.vx = Math.random() * 4 - 2 ; // speed
+    this.vy = Math.random() * 4 - 2 ;
   }
 
   draw(context) {
+    // I want wach particle to have a different color. I have to define "fillStyle" and change this setting every time we are about to draw anew particle.
+    // use hsl() color declaration for multiple colors
+    context.fillStyle = 'hsl(' + this.x * 0.5 +', 100%, 50%)'; //  360 degree in the color wheel, try to use 'hsl(' + this.x * 0.5 +', 100%, 50%)' or 'hsl(' + Math.random() * 360 +', 100%, 50%)'
     context.beginPath();
     // to draw a circle on canvas we start by calling begin path to tell JS to draw a new shape built in .arc() method
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2 ); // arc() just defines a path,doesn't draw anything. 0 =  start angle, angle of Math.PI - full circular arc
     context.fill(); // draw the circle
+    context.stroke();
+  }
 
+  update() {
+    this.x += this.vx;
+    if (this.x > this.effect.width - this.radius || this.x < this.radius) this.vx *= -1;
+
+    this.y += this.vy;
+    if (this.y > this.effect.height - this.radius || this.y < this.radius) this.vy *= -1;
   }
 }
 
@@ -38,7 +51,7 @@ class Effect {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.particles = []; // particles will be stored here 
-    this.numberOfParticles = 20;
+    this.numberOfParticles = 200;
     this.createParticles();
   }
 
@@ -53,14 +66,19 @@ class Effect {
   handleParticles(context) {
     this.particles.forEach((particle) => {
       particle.draw(context);
+      particle.update();
     })
   }
 }
 const effect = new Effect(canvas);
 console.log(effect);
 
-effect.handleParticles(ctx);
+
 
 function animate() {
-  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  effect.handleParticles(ctx);
+  requestAnimationFrame( animate )
 } 
+
+animate();
