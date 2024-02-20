e1aa378a5315c4c2289a43dd38963edc60d61ec6
nataliia-v <nataly.verbenskaya@gmail.com>
2024-02-20 14:46:54 +0200

    connect particles

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ v
  ## CanvasRenderingContext2D 
  CanvasRenderingContext2D provides the 2D rendering context for the drawing surface of a <canvas> element.
  It is used for drawing shapes, text, images, and other objects.
+
+
+## How to calculate the distance between two points in 2d space?
+[Look at the connectParticles() method ](/script.js)
+Very important coding technique for code for physics simulations,games, or creative coding projects.
+To get the distance value we need to imagine there is a right triangle in between them. This will work regardless of
+where in whick position relative to each other particleA and particleB are we calculate [DX distance](/images/dx_distance.jpg) between 
+particleA and particleB on the horizontal x AIS. We calculate [DY distance](/images/dy_distance.jpg) between particleA and particleB on the vertical y AIS. We know DX, DY, right angle 90. Knowing all of this we can use Pythagoras Theorem formula to calculate [hypotenuse](/images/hypotenuse.jpg) (the longest side of the right triangle) which is also the actual distance between two points (particleA and particleB ) in [connectParticles() method ](/script.js) const distance. 
--- script.js
+++ script.js
@@ -4,23 +4,25 @@ const ctx = canvas.getContext('2d'); // create an instance CanvasRenderingContex
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-console.log(ctx);
 const gradient = ctx.createLinearGradient(0,0,canvas.width, canvas.height)
 gradient.addColorStop(0, 'white');
 gradient.addColorStop(0.5, 'magenta');
 gradient.addColorStop(1, 'blue');
 ctx.fillStyle = gradient;
+ctx.strokeStyle = 'white';
+
+console.log(ctx);
 
 
 class Particle {
 
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 40 + 5;
+    this.radius = Math.random() * 5 + 2;
     this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2); // "this.radius +" to full visible particle at the border(left)  " - this.radius * 2" to full visible particle at the border(right)
     this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2) // "this.radius +" to full visible particle at the border(top)  " - this.radius * 2" to full visible particle at the border(bottom)
-    this.vx = Math.random() * 4 - 2 ; // speed
-    this.vy = Math.random() * 4 - 2 ;
+    this.vx = Math.random() * 1 - 0.5 ; // speed
+    this.vy = Math.random() * 1 - 0.5 ;
   }
 
   draw(context) {
@@ -64,10 +66,40 @@ class Effect {
   }
 
   handleParticles(context) {
+    this.connectParticles(context)
+
     this.particles.forEach((particle) => {
       particle.draw(context);
       particle.update();
     })
+  }
+
+  connectParticles(context) {
+    const maxDistance = 100;
+    // compare every particle inside particles array
+    for( let particleA = 0; particleA < this.particles.length; particleA++ ){
+      // we can calculate particles distance and check if they are less than 100 pixels
+      //Cycle over the entire particles array and for each of those particles run over the same array again to compare each particle  
+      for(let particleB = particleA; particleB < this.particles.length; particleB++) {
+        // comparing two particles at the time untill we compared all of them.
+        // Always be comparing particle object A in the array against another particle aobject B in the same array.
+        // calculate the distance between this two points(in 2d space) (Read in README):
+
+        const dx = this.particles[particleA].x - this.particles[particleB].x;
+        const dy = this.particles[particleA].y - this.particles[particleB].y;
+        const distance = Math.hypot(dx, dy);
+
+        if (distance < maxDistance) {
+          context.save();
+          const opacity = 1 - (distance/maxDistance);
+          context.globalAlpha = opacity;
+          context.beginPath();
+          context.moveTo(this.particles[particleA].x, this.particles[particleA].y);
+          context.lineTo(this.particles[particleB].x, this.particles[particleB].y);
+          context.stroke();
+        }
+      }
+    }
   }
 }
 const effect = new Effect(canvas);
