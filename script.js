2c52cda83cb824e5ee7812308a1c27a127bd9390
nataliia-v <nataly.verbenskaya@gmail.com>
2024-02-22 14:14:07 +0200

    added resize and move particles from mouse

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ class Particle {
 
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 5 + 2;
+    this.radius = Math.random() * 5 + 5;
     this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2); // "this.radius +" to full visible particle at the border(left)  " - this.radius * 2" to full visible particle at the border(right)
     this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2) // "this.radius +" to full visible particle at the border(top)  " - this.radius * 2" to full visible particle at the border(bottom)
     this.vx = Math.random() * 1 - 0.5 ; // speed
@@ -34,11 +34,27 @@ class Particle {
   }
 
   update() {
+    if(this.effect.mouse.pressed){
+      const dx = this.x - this.effect.mouse.x;// calculete the distasnce between particle and mouse (dx and dy)
+      const dy = this.y - this.effect.mouse.y;
+      const distance = Math.hypot(dx, dy);
+      if(distance < this.effect.mouse.radius){ // push particle from mouse
+        const angle = Math.atan2(dy, dx); // gives countreclockwise angle in radians, between the positive x-axis and a line projected from point 0,0 towards specificx and y coordinates (target points)
+        this.x += Math.cos(angle);
+        this.y += Math.sin(angle); 
+      }
+    };
+
     this.x += this.vx;
     if (this.x > this.effect.width - this.radius || this.x < this.radius) this.vx *= -1;
 
     this.y += this.vy;
     if (this.y > this.effect.height - this.radius || this.y < this.radius) this.vy *= -1;
+  }
+
+  reset() {
+    this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
+    this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2) 
   }
 }
 
@@ -48,13 +64,44 @@ class Particle {
 // Effect is a store of all custom settings and the helper methods 
 class Effect {
   // width and height avaliable canvas area. We need to be sure, that we draw our shapes only within the canvas area and not somewere of screen. 
-  constructor (canvas) {
+  constructor (canvas, context) {
     this.canvas = canvas;
+    this.context = context;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.particles = []; // particles will be stored here 
     this.numberOfParticles = 200;
     this.createParticles();
+
+    this.mouse = {
+      x: 0,
+      y: 0,
+      pressed: false, // pressed button
+      radius: 150, // mouse radius
+    }
+
+    window.addEventListener('resize', e => {
+      this.resize(e.target.window.innerWidth, e.target.window.innerHeight)
+    })
+
+    window.addEventListener('mousemove', e => {
+      if(this.mouse.pressed) {
+        this.mouse.x = e.x;
+        this.mouse.y = e.y;
+      }
+
+    } );
+
+    window.addEventListener('mousedown', e => {
+      this.mouse.pressed = true;
+      this.mouse.x = e.x;
+      this.mouse.y = e.y;
+    } )
+
+    window.addEventListener('mouseup', e => {
+      this.mouse.pressed = false;
+    } )
+
   }
 
   createParticles() {
@@ -66,12 +113,13 @@ class Effect {
   }
 
   handleParticles(context) {
-    this.connectParticles(context)
-
+    this.connectParticles(context);
     this.particles.forEach((particle) => {
       particle.draw(context);
       particle.update();
     })
+
+   
   }
 
   connectParticles(context) {
@@ -90,19 +138,37 @@ class Effect {
         const distance = Math.hypot(dx, dy);
 
         if (distance < maxDistance) {
-          context.save();
+          // context.save(); ?
           const opacity = 1 - (distance/maxDistance);
           context.globalAlpha = opacity;
           context.beginPath();
           context.moveTo(this.particles[particleA].x, this.particles[particleA].y);
           context.lineTo(this.particles[particleB].x, this.particles[particleB].y);
           context.stroke();
+          // context.restore(); ?
         }
       }
     }
   }
+
+  resize(width, height) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.width = width;
+    this.height - height;
+
+    const gradient = this.context.createLinearGradient(0,0,width, height)
+    gradient.addColorStop(0, 'white');
+    gradient.addColorStop(0.5, 'magenta');
+    gradient.addColorStop(1, 'blue');
+    this.context.fillStyle = gradient;
+    this.context.strokeStyle = 'white';
+    this.particles.forEach((particle) => {
+      particle.reset();
+    } )
+  }
 }
-const effect = new Effect(canvas);
+const effect = new Effect(canvas, ctx);
 console.log(effect);
 
 
